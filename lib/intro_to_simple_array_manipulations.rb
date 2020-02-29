4da9eb4b82f1eae76b1ea90134522ce536cdac6a
Richard Bobo <richard.bobo@gmail.com>
2020-02-29 19:15:12 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_push(array, string)
+ array = ["John", "Richard", "Michael"]
+  string = "violet"
+  array.push(string)
+ 
+end
+
+
+def using_unshift(bouroughs_in_nyc, new_neighborhood)
+    bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+    new_neighborhood = "Staten Island"
+   bouroughs_in_nyc.unshift(new_neighborhood)
+   p bouroughs_in_nyc
+ end
+   
+   
+
+def using_pop(continents)
+  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  deleted_string = continents.pop
+
+ end
+   
