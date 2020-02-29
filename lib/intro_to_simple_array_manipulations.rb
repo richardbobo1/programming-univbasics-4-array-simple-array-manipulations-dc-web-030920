ee07e8912c6e33b875dcb887ee8e70ebc666886a
Richard Bobo <richard.bobo@gmail.com>
2020-02-29 19:34:27 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -43,7 +43,7 @@ end
 
 def using_concat(my_favorite_things, more_favs)
   
-  my_favorite_things.concat 
+  my_favorite_things.concat more_favs
   
   
    all_my_favs = .concat(@my_favorite_things, more_favs)
