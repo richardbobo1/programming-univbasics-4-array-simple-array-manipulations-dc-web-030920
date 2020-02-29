908bfe7fbccec501d05b3f909df6168073ae8022
Richard Bobo <richard.bobo@gmail.com>
2020-02-29 19:41:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -59,49 +59,8 @@ end
 
 
 
-    @another_language = "Python"
-    @new_array = using_insert(@list_of_programming_languages, @another_language)
-
 
 ############
-
-
-
-
-def using_uiniq(array)
-  .uniq 
-  
-end
-
-
-
-
-
-def using_flatten(array)
-  .flatten 
-  
-end
-
-
-
-
-
-def using_delete(array, string)
-  .delete 
-  
-end
-
-
-
-
-def using_delete_at(array, integer)
-  
-end
-
-  
-
-
-
 
 
 
